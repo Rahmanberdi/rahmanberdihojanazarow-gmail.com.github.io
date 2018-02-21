e13c2f20ff578a7eb10e66d2c2fdb00e2dab0537
Rahmanberdi <33062050+Rahmanberdi@users.noreply.github.com>
2018-02-21 12:27:37 +0500

    First website upload

---
+++ js/alert.js
@@ -0,0 +1,28 @@
+function sum() 
+	{
+	var a = prompt("enter a first number");
+	var b = prompt("enter a second number");
+	var c = alert(a-(-b));
+	}
+
+	function sub() 
+	{
+	var a = prompt("enter a first number");
+	var b = prompt("enter a second number");
+	var c = alert(a-b);
+	}	
+
+	function mul() 
+	{
+	var a = prompt("enter a first number");
+	var b = prompt("enter a second number");
+	var c = alert(a*b);
+	}
+
+	function div() 
+	{
+	var a = prompt("enter a first number");
+	var b = prompt("enter a second number");
+	var c = alert(a/b);
+		 												
+	}
